2a6cdb5afe06899bd6f415c6516887dc61e3138c
Nate Spencer <nate@natespencer.com>
2018-04-21 19:36:05 -0500

    add build to test
    add build to test script

--- fib_aas/run_unit_test.sh
+++ fib_aas/run_unit_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo 'Starting Docker Compose in Daemon mode if not already started'
-docker-compose up -d
+docker-compose up --build -d
 docker-compose exec web python tests/test_fib.py
 echo 'Tests complete'
 echo 'run docker-compose down to shutdown docker stack'
